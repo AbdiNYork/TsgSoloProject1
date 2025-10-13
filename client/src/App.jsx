b70c266684fbf5016504e30e1617a176e8a8fa6c
AbdiNYork <AnurC@Yorksolutions.net>
2025-10-13 12:37:57 -0400

    made a change on dev - should be a conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-        and make some changes
+        and make some more changes
       </p>
     </>
   )
